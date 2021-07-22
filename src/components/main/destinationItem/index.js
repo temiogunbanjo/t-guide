c75b3d435c3249996c04cf06d480ad42f06bfccf
tehmi2000 <tehmi2000@gmail.com>
2021-07-22 19:27:25 +0100

    Added google tag manager

--- src/components/main/destinationItem/index.js
+++ src/components/main/destinationItem/index.js
@@ -9,7 +9,7 @@ function Destination(props){
         <div className="cols destination">
             <img src={ props.details.image } alt={ props.details.name.toLowerCase() }/>
             <div className="cols content">
-                <span className="name" style={{ textAlign: "left", fontSize: "20px", fontWeight: "500", textTransform: "capitalize", margin: "0 0 0.5rem"}}>{props.name}</span>
+                <span className="name" style={{ textAlign: "left", fontSize: "20px", fontWeight: "500", textTransform: "capitalize", margin: "0 0 0.5rem"}}>{props.details.name}</span>
                 <span className="line-clamp line-clamp-5 description">{ props.details.description }</span>
                 <div className="rows button-container">
                     <button style={ buttonStyle } onClick={ props.handler } data-id="4" data-options={ destinationDetails }>Explore</button>
