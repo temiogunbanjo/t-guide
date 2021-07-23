25145a350c09e520166bcc1e58a0ab422d0c774c
tehmi2000 <tehmi2000@gmail.com>
2021-07-23 13:26:41 +0100

    Added dummy profile picture and map link

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -4,7 +4,7 @@ import Navigation from '../navigationBar';
 import Destination from './destinationItem';
 import GuideItem from './guideitem';
 import pic from '../../resources/images/julian-schultz-JP_nc0jEakE-unsplash.jpg';
-import aVideo from '../../resources/videos/Ghana Drone Tour.mp4'; // Dummy example
+import profilePic from '../../resources/images/random picture for all.jpg';
 
 // Contains all destinations and their details
 const DESTINATIONS = [
@@ -12,15 +12,14 @@ const DESTINATIONS = [
         image: "https://static.wixstatic.com/media/d0a4b2_9ea4562bbfc94a5c991ec810637f7bb5.png/v1/fill/w_740,h_494,al_c,lg_1,q_95/d0a4b2_9ea4562bbfc94a5c991ec810637f7bb5.webp",
         video: "https://www.youtube.com/embed/CmeqLpbj0I4",
         name: "Vume Pottery",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum assumenda ut, facilis, soluta autem vero earum suscipit consectetur in officiis sint possimus adipisci velit eos. Quis quos eius excepturi eveniet?",
-        location: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7941.2907345136855!2d-0.2279730982244871!3d5.6192666896778185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf99519d280a59%3A0xd2c0850c11243fa4!2sGH%20Schools!5e0!3m2!1sen!2sgh!4v1616658821973!5m2!1sen!2sgh"
+        description: "Vume is a small town in the South Tongu District of the Volta Region near Sogakope. The residents of Vume are well noted for the art of pottery."
     },
     {
         image: "https://static.wixstatic.com/media/d0a4b2_9ea4562bbfc94a5c991ec810637f7bb5.png/v1/fill/w_740,h_494,al_c,lg_1,q_95/d0a4b2_9ea4562bbfc94a5c991ec810637f7bb5.webp",
         video: "", // can also be a link to a video
         name: "Kyabobo",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum assumenda ut, facilis, soluta autem vero earum suscipit consectetur in officiis sint possimus adipisci velit eos. Quis quos eius excepturi eveniet?",
-        location: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7941.2907345136855!2d-0.2279730982244871!3d5.6192666896778185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf99519d280a59%3A0xd2c0850c11243fa4!2sGH%20Schools!5e0!3m2!1sen!2sgh!4v1616658821973!5m2!1sen!2sgh"
+        location: ""
     }, {
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Inside_view_of_the_Fort_Prinzenstein_in_Keta%2C_Ghana._9.jpg/320px-Inside_view_of_the_Fort_Prinzenstein_in_Keta%2C_Ghana._9.jpg",
         video: "",
@@ -130,7 +129,7 @@ function DestinationDetails(props) {
 
             <h4 style={{ width: "100%", textAlign: "left", letterSpacing: "0" }}>Location:</h4>
             <div className="iframe-wrapper">
-                <iframe src={ parsedDetails.location } className="virtual-tour-iframe" crossOrigin></iframe>
+                <iframe src={ parsedDetails.location } className="virtual-tour-iframe" width="600" height="450" style={{border: "0"}} allowFullScreen="" loading="lazy"></iframe>
             </div>
             
             <button onClick={ props.navigationHandler } data-id="2">Get A Tour Guide</button>
@@ -176,24 +175,24 @@ function GuideDetails(props) {
 function GuideSection(props) {
     const items = [
         {
-            image: "https://static.wixstatic.com/media/d0a4b2_9ea4562bbfc94a5c991ec810637f7bb5.png/v1/fill/w_740,h_494,al_c,lg_1,q_95/d0a4b2_9ea4562bbfc94a5c991ec810637f7bb5.webp",
+            image: profilePic,
             name: "Mr Person 1",
             description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum assumenda ut, facilis, soluta autem vero earum suscipit consectetur in officiis sint possimus adipisci velit eos. Quis quos eius excepturi eveniet?"
         },
         {
-            image: "https://static.wixstatic.com/media/d0a4b2_9ea4562bbfc94a5c991ec810637f7bb5.png/v1/fill/w_740,h_494,al_c,lg_1,q_95/d0a4b2_9ea4562bbfc94a5c991ec810637f7bb5.webp",
+            image: profilePic,
             name: "Mr Person 2",
             description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum assumenda ut, facilis, soluta autem vero earum suscipit consectetur in officiis sint possimus adipisci velit eos. Quis quos eius excepturi eveniet?"
         }, {
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Inside_view_of_the_Fort_Prinzenstein_in_Keta%2C_Ghana._9.jpg/320px-Inside_view_of_the_Fort_Prinzenstein_in_Keta%2C_Ghana._9.jpg",
+            image: profilePic,
             name: "Mr Person 3",
             description: "Lorem ipsum dolor colr et sor skskkdk sdndjnkdnknkds md nkdknjksdk"
         }, {
-            image: pic,
+            image: profilePic,
             name: "Mr Person 4",
             description: "Lorem ipsum dolor colr et sor skskkdk sdndjnkdnknkds md nkdknjksdk"
         }, {
-            image: pic,
+            image: profilePic,
             name: "Mr Person 5",
             description: "Lorem ipsum dolor colr et sor skskkdk sdndjnkdnknkds md nkdknjksdk"
         }
