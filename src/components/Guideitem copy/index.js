41a41f6838d0ffd7f15f5db3a5c664eae9410c9d
tehmi2000 <tehmi2000@gmail.com>
2021-09-08 10:52:56 +0100

    Removed folders in common

---
+++ src/components/Guideitem copy/index.js
@@ -0,0 +1,51 @@
+import "./guideitem.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "../../common/Ratings";
+
+function GuideItem(props) {
+  const nameElementStyle = {
+    textAlign: "left",
+    fontSize: "20px",
+    fontWeight: "500",
+    textTransform: "capitalize",
+    margin: "0 0 0.5rem",
+  };
+  const buttonStyle = {
+    fontSize: "13px",
+    padding: "1.2rem 2rem",
+  };
+  const { details } = props;
+
+  return (
+    <div className="cols guide-item">
+      <img
+        src={details.image}
+        alt={details.name.toLowerCase()}
+        className="rows center"
+      />
+      <div className="cols content">
+        <span className="name" style={nameElementStyle}>
+          {details.name}
+        </span>
+        <span className="line-clamp line-clamp-5 description">
+          {details.description}
+        </span>
+        <Rating rating={details.rating} styles={{marginTop: '0.8rem'}}/>
+        <div className="rows button-container">
+          <Link
+            to={`/guides/${details.uuid}`}
+            className="btn more-button"
+            style={buttonStyle}
+            onClick={props.handler}
+            data-show={false}
+          >
+            View Profile
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default GuideItem;
